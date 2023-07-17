a17e0f2f4447f24cb154d652e5f05c111b29c272
Steven Borik <98104308+StevenJBorik@users.noreply.github.com>
2023-07-16 23:08:58 -0500

    audio play functionality

--- packages/key-finder-web/src/AudioFileKeyDetection/AudioFileItem.tsx
+++ packages/key-finder-web/src/AudioFileKeyDetection/AudioFileItem.tsx
@@ -30,9 +30,14 @@ interface State {
   keySignatureNumericValue: number | null;
   fileItem: FileItem;
   scale: { [key: string]: number[] } | null;
+  isPlaying: boolean;
+  currentTime: number;
+  source: AudioBufferSourceNode | null;
+  playingSource: AudioBufferSourceNode | null;
 }
 
 class AudioFileItem extends Component<Props, State> {
+  audioElement: HTMLAudioElement = new Audio();
   worker: Worker | null = null;
   terminated: boolean = false;
 
@@ -46,7 +51,14 @@ class AudioFileItem extends Component<Props, State> {
     keySignatureNumericValue: null,
     fileItem: this.props.fileItem,
     scale: null,
+    isPlaying: false,
+    currentTime: 0,
+    source: null,
+    playingSource: null,
   };
+
+  audioContext: AudioContext | null = null;
+  source: AudioBufferSourceNode | null = null;
 
   componentDidMount() {
     if (this.props.fileItem.canProcess) {
@@ -54,6 +66,13 @@ class AudioFileItem extends Component<Props, State> {
       reader.onload = this.handleFileLoad;
       reader.readAsArrayBuffer(this.props.fileItem.file);
     }
+
+    // Create the audio context here and use it for playback later
+    this.audioContext = new (window.AudioContext ||
+      (window as any).webkitAudioContext)();
+    this.audioElement.addEventListener('error', (error) => {
+      console.error('Audio playback error:', error);
+    });
   }
 
   componentDidUpdate(prevProps: Props) {
@@ -69,25 +88,85 @@ class AudioFileItem extends Component<Props, State> {
 
   componentWillUnmount() {
     this.worker?.terminate();
+    this.audioElement.pause();
+    this.audioElement.removeEventListener(
+      'timeupdate',
+      this.handleAudioTimeUpdate
+    );
+    this.audioElement.removeEventListener('error', () => {});
+
+    // Close the audio context when the component is unmounted
+    if (this.audioContext) {
+      this.audioContext.close();
+      this.audioContext = null;
+    }
   }
+
+  handleAudioPlayPause = () => {
+    if (!this.audioContext || !this.state.source) return;
+
+    const { source, playingSource, isPlaying, currentTime } = this.state;
+
+    if (isPlaying) {
+      this.audioContext.suspend().then(() => {
+        this.setState({ isPlaying: false });
+        source.stop();
+      });
+    } else {
+      this.audioContext.resume().then(() => {
+        if (playingSource) {
+          // Stop the currently playing source if it exists
+          playingSource.stop();
+        }
+        this.setState({ isPlaying: true });
+        const newSource = this.audioContext.createBufferSource();
+        newSource.buffer = source.buffer;
+        newSource.connect(this.audioContext.destination);
+        newSource.onended = () => {
+          this.setState({ isPlaying: false, playingSource: null });
+        };
+        this.setState({ playingSource: newSource });
+        newSource.start(0, currentTime); // Start the new source node from the current time
+      });
+    }
+  };
+
+  handleAudioTimeUpdate = () => {
+    this.setState({ currentTime: this.audioElement.currentTime });
+  };
+
+  handleAudioSeek = (event: Event) => {
+    if (!this.audioElement) return;
+    const seekTime = (event.target as HTMLInputElement).valueAsNumber;
+    this.audioElement.currentTime = seekTime;
+    this.setState({ currentTime: seekTime });
+  };
 
   handleAudioFile = async (buffer: AudioBuffer) => {
     console.log('AudioFileItem - handleAudioFile');
-    const sampleRate = buffer.sampleRate;
-    const numberOfChannels = buffer.numberOfChannels;
-    const channelData: Float32Array[] = [];
-    for (let i = 0; i < numberOfChannels; i += 1) {
-      channelData.push(buffer.getChannelData(i));
-    }
+
+    const context = this.audioContext; // Use the existing audio context
+    if (!context) return;
+
+    const source = context.createBufferSource();
+    source.buffer = buffer;
+    source.connect(context.destination);
+    source.onended = () => {
+      this.setState({ isPlaying: false });
+    };
 
     this.setState({ analyzing: true });
 
     const worker = keyFinderUtils.initializeKeyFinder({
-      sampleRate,
-      numberOfChannels,
+      sampleRate: buffer.sampleRate,
+      numberOfChannels: buffer.numberOfChannels,
     });
 
-    const segmentCounts = Math.floor(channelData[0].length / sampleRate);
+    const channelData: Float32Array[] = [];
+    for (let i = 0; i < buffer.numberOfChannels; i += 1) {
+      channelData.push(buffer.getChannelData(i));
+    }
+    const segmentCounts = Math.floor(channelData[0].length / buffer.sampleRate);
 
     worker.addEventListener('message', (event) => {
       if (event.data.finalResponse) {
@@ -115,8 +194,8 @@ class AudioFileItem extends Component<Props, State> {
           this.advanceSegmentCount(
             worker,
             channelData,
-            sampleRate,
-            numberOfChannels,
+            buffer.sampleRate,
+            buffer.numberOfChannels,
             0
           );
         } else {
@@ -127,12 +206,12 @@ class AudioFileItem extends Component<Props, State> {
           this.setState({ result });
 
           if (this.state.currentSegment < segmentCounts) {
-            const offset = this.state.currentSegment * sampleRate;
+            const offset = this.state.currentSegment * buffer.sampleRate;
             this.advanceSegmentCount(
               worker,
               channelData,
-              sampleRate,
-              numberOfChannels,
+              buffer.sampleRate,
+              buffer.numberOfChannels,
               offset
             );
           } else {
@@ -146,10 +225,14 @@ class AudioFileItem extends Component<Props, State> {
     this.advanceSegmentCount(
       worker,
       channelData,
-      sampleRate,
-      numberOfChannels,
+      buffer.sampleRate,
+      buffer.numberOfChannels,
       0
     );
+
+    // Start audio playback after the context is created and everything is set up
+    source.start();
+    this.setState({ source, isPlaying: true });
   };
 
   advanceSegmentCount = (
@@ -184,7 +267,8 @@ class AudioFileItem extends Component<Props, State> {
   }
 
   handleFileLoad = async (event: ProgressEvent<FileReader>): Promise<void> => {
-    const context = audioUtils.createAudioContext();
+    const context = new (window.AudioContext ||
+      (window as any).webkitAudioContext)();
     if (!crypto?.subtle?.digest) {
       console.error(
         'Web Cryptography API is not supported in this environment.'
@@ -200,12 +284,18 @@ class AudioFileItem extends Component<Props, State> {
       .map((b) => b.toString(16).padStart(2, '0'))
       .join('');
     this.props.updateDigest(this.props.fileItem.id, hashHex);
-    console.log('AudioFileItem - handleFileLoad');
-    context.decodeAudioData(event.target.result as ArrayBuffer, (buffer) => {
-      this.handleAudioFile(buffer);
-    });
-  };
 
+    console.log('AudioFileItem - handleFileLoad');
+    context.decodeAudioData(
+      event.target.result as ArrayBuffer,
+      (buffer) => {
+        this.handleAudioFile(buffer);
+      },
+      (error) => {
+        console.error('Error decoding audio data:', error);
+      }
+    );
+  };
   renderFretboardScale(normalizedResult: string | null) {
     if (!normalizedResult) {
       return;
@@ -221,9 +311,8 @@ class AudioFileItem extends Component<Props, State> {
   }
 
   render() {
-    console.log('AudioFileItem - render');
     const { fileItem } = this.props;
-    const { analyzing, result } = this.state;
+    const { analyzing, result, isPlaying, currentTime } = this.state;
 
     return (
       <div class="file-item__container">
@@ -237,6 +326,30 @@ class AudioFileItem extends Component<Props, State> {
               <div id="fretboard-container"></div>
             </div>
           )}
+
+          {/* Audio Player */}
+          <div>
+            {fileItem.file && (
+              <audio
+                ref={(el) => (this.audioElement = el)}
+                src={URL.createObjectURL(fileItem.file)}
+                controls // Add the controls attribute here
+                onTimeUpdate={this.handleAudioTimeUpdate}
+              />
+            )}
+            <button onClick={this.handleAudioPlayPause}>
+              {isPlaying ? 'Pause' : 'Play'}
+            </button>
+            {fileItem.file && (
+              <input
+                type="range"
+                min={0}
+                max={this.audioElement?.duration || 0}
+                value={currentTime}
+                onChange={this.handleAudioSeek}
+              />
+            )}
+          </div>
         </div>
       </div>
     );
