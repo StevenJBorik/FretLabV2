77478adb49f9de5e72ced51b804baa3e2c78fe21
Steven Borik <stevenjborik@protonmail.com>
2023-12-18 23:30:42 -0600

    untested api calls for settings.tsx

--- packages/key-finder-web/src/Settings.tsx
+++ packages/key-finder-web/src/Settings.tsx
@@ -1,12 +1,9 @@
-// settings.tsx
-
-import { useState } from 'preact/hooks';
+import { useState, useEffect } from 'preact/hooks';
 import { route } from 'preact-router';
 import { h, FunctionComponent } from 'preact';
-// import './Settings.css'; // Assume we have some CSS for styling
 
 interface SettingsProps {
-  path: string; // Add this if you're passing 'path' as a prop
+  path: string;
 }
 
 const Settings: FunctionComponent<SettingsProps> = ({ path }) => {
@@ -14,74 +11,58 @@ const Settings: FunctionComponent<SettingsProps> = ({ path }) => {
   const [profileVisibility, setProfileVisibility] = useState('private');
   const [receiveEmails, setReceiveEmails] = useState(true);
 
-  const handleEmailChange = (e: Event) => {
-    setEmail((e.target as HTMLInputElement).value);
-  };
+  useEffect(() => {
+    // Fetch user settings on component mount
+    fetch('/api/settings', {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setEmail(data.email);
+        setProfileVisibility(data.profileVisibility);
+        setReceiveEmails(data.receiveEmails);
+      });
+  }, []);
 
-  const handleProfileVisibilityChange = (e: Event) => {
-    setProfileVisibility((e.target as HTMLSelectElement).value);
-  };
-
-  const handleReceiveEmailsChange = (e: Event) => {
-    setReceiveEmails((e.target as HTMLInputElement).checked);
+  const updateSettings = (endpoint, data) => {
+    return fetch(endpoint, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+      body: JSON.stringify(data),
+    });
   };
 
   const saveDetails = () => {
-    console.log('Save settings');
-    // Here you would usually make an API call to save the settings
+    Promise.all([
+      updateSettings('/api/settings/email', { email }),
+      updateSettings('/api/settings/profile-visibility', { profileVisibility }),
+      updateSettings('/api/settings/email-subscription', { receiveEmails }),
+    ])
+      .then(() => {
+        console.log('Settings updated');
+        // Inform the user of the update, e.g., using a toast notification
+      })
+      .catch((error) => {
+        console.error('Error updating settings:', error);
+      });
   };
 
-  const goToChangePassword = () => {
+  const handleChangePassword = () => {
+    // Implement change password logic
     route('/change-password');
-  };
-
-  const goToUpdateEmail = () => {
-    route('/update-email');
   };
 
   return (
     <div class="settings-container">
       <h1>Account Settings</h1>
-      <div class="setting">
-        <label for="email">Email Address</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onInput={handleEmailChange}
-        />
-      </div>
-      <div class="setting">
-        <label for="profile-visibility">My Profile</label>
-        <select
-          id="profile-visibility"
-          value={profileVisibility}
-          onChange={handleProfileVisibilityChange}
-        >
-          <option value="private">Private</option>
-          <option value="public">Public</option>
-        </select>
-      </div>
-      <div class="setting">
-        <label for="receive-emails">
-          <input
-            type="checkbox"
-            id="receive-emails"
-            checked={receiveEmails}
-            onChange={handleReceiveEmailsChange}
-          />
-          Receive email notifications
-        </label>
-      </div>
-      <div class="links">
-        <a href="#" onClick={goToChangePassword}>
-          Change Password
-        </a>
-        <a href="#" onClick={goToUpdateEmail}>
-          Update Email Address
-        </a>
-      </div>
+      {/* ...other settings code */}
       <button onClick={saveDetails}>Save Details</button>
+      <button onClick={handleChangePassword}>Change Password</button>
     </div>
   );
 };
